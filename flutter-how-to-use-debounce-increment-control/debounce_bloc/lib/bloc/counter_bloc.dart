c8d21632927daae6b0230b3d7f8e1860109170a7
Iain Smith <b099l3@gmail.com>
2022-10-07 23:44:44 +0100

    [flutter-how-to-use-debounce-increment-control] With debounce

--- flutter-how-to-use-debounce-increment-control/debounce_bloc/lib/bloc/counter_bloc.dart
+++ flutter-how-to-use-debounce-increment-control/debounce_bloc/lib/bloc/counter_bloc.dart
@@ -1,15 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 part 'counter_bloc.freezed.dart';
 part 'counter_event.dart';
 part 'counter_state.dart';
+
+EventTransformer<Event> debounce<Event>({
+  Duration duration = const Duration(milliseconds: 300),
+}) {
+  return (events, mapper) => events.debounce(duration).switchMap(mapper);
+}
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc({required int initialValue})
       : super(CounterState.loaded(counter: initialValue)) {
     on<CounterChanged>(
       _onCounterChanged,
+      transformer: debounce(),
     );
   }
 
